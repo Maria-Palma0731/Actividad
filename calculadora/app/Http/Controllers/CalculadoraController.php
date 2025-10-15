cb4ce0d5d8f0e4f6ee6fb254e671db7bbb9e8ba7
Maria-Palma0731 <123046449@upq.edu.mx>
2025-10-15 14:50:38 -0600

    Operaciones(Suma, Resta)

--- calculadora/app/Http/Controllers/CalculadoraController.php
+++ calculadora/app/Http/Controllers/CalculadoraController.php
@@ -6,5 +6,24 @@ use Illuminate\Http\Request;
 
 class CalculadoraController extends Controller
 {
-    //
+    public function mostrar(Request $request)
+    {
+        $resultado = null;
+
+        if ($request->isMethod('post')) {
+            $a = $request->input('num1');
+            $b = $request->input('num2');
+            $op = $request->input('operacion');
+
+            switch ($op) {
+                case 'suma': $resultado = $a + $b; break;
+                case 'resta': $resultado = $a - $b; break;
+
+            }
+        }
+
+        return view('calculadora', compact('resultado'));
+    }
+
 }
+
